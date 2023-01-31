c95f39ea4056aff4be9bd80569222aa7aca26737
Anastasya <ocenka.da@gmail.com>
2023-01-31 14:42:56 +0700

    JS

---
+++ assets/script/js.js
@@ -0,0 +1,7 @@
+greeting = function (userName) {return userName}
+{
+    userName = prompt("Пожалуйста, введите Ваше имя");
+    alert("Привет " + userName + "!" );
+}
+greeting();
+//function Вашеимяфункциитут()
